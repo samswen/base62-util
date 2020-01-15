2de31c412e50bfd07a2469c2259e1b959e2fb745
Sam Wen <samw@ritani.com>
2020-01-15 17:07:37 -0500

    add base62_encode_buffer for convience

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const BASE62='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const base62 = require('base-x')(BASE62);
 
 function base62_encode(str) {
-    return base62.encode(Buffer.from(str));
+    return base62_encode_buffer(Buffer.from(str));
+}
+
+function base62_encode_buffer(buffer) {
+    return base62.encode(buffer);
 }
 
 function base62_decode(str) {
@@ -14,4 +18,5 @@ function base62_decode(str) {
 module.exports = {
     base62_encode,
     base62_decode,
+    base62_encode_buffer,
 }
--- test/test.js
+++ test/test.js
@@ -1,18 +1,24 @@
 /* eslint-disable no-undef */
 'use strict';
 
-const { base62_encode, base62_decode } = require('../src');
+const { base62_encode, base62_decode, base62_encode_buffer } = require('../src');
 
 const chai = require('chai');
 const assert = chai.assert;
 const expect = chai.expect;
 
-describe('Test base62_encode and base62_decode', () => {
+describe('Test base62_encode, base62_decode and encoded_buffer', () => {
 
     it('verifies a string after encoding and decoding are still the same', async () => {
 
+
         const str = 'Hello base62 encode/decode';
+
+        const encoded_buffer = base62_encode_buffer(Buffer.from(str));
+
         const encoded = base62_encode(str);
+
+        expect(encoded_buffer).equals(encoded);
 
         //console.log(encoded);
         assert.isNotNull(encoded);
